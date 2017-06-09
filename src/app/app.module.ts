54782166320c6ee51aeebb954bc8f39fd01c397a
Taras <murzenkovtn@gmail.com>
2017-06-09 14:40:22 +0300

    added boostrap css lib, added nabart component, added task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {TaskComponent} from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
