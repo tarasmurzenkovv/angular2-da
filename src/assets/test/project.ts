463ad94d53368242503232c01de945dd4727c584
Taras <murzenkovtn@gmail.com>
2017-06-16 12:12:54 +0300

    added unit tests

---
+++ src/assets/test/project.ts
@@ -0,0 +1,14 @@
+import {Project} from '../../app/model/project';
+
+export const project: Project = {
+  Name: 'OpenQ',
+  Description: 'The complete solution for Medical Affairs organizations.',
+  CustomerName: 'OpenQ',
+  StartDate: null,
+  EndDate: null,
+  ImageUrl: 'project.png',
+  Tickets: [],
+  Id: 944
+}
+
+
